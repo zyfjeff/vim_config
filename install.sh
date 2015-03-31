bc17a40c3d242dabaf5d31bfe9f145f8437ac5ca
zyfjeff <445188383@qq.com>
2015-03-31 05:56:02 +0100

    安装脚本

---
+++ install.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+# vim配置文件安装
+
+if [ -e "/etc/redhat-release" ];then
+    #是 redhat系列
+    which ctags &>/dev/null
+    if [ ! $? -eq 0 ];then
+        #不存在ctags可以直接安装
+        yum install ctags -y
+        if [ $? -eq 0 ];then
+            mkdir -p ~/.vim/ &>/dev/null
+            \cp -rf ./.vimrc ~/ &>/dev/null
+            \cp -rf ./* ~/.vim/ &>/dev/null
+        else
+            echo "需要手动安装ctags"
+        fi
+
+    fi
+else
+    #是 ubuntu系列
+    which ctags &>/dev/null
+    if [ ! $? -eq 0 ];then
+        sudo apt-get install ctags -y
+        if [ $? -eq 0 ];then
+            mkdir -p ~/.vim/ &>/dev/null
+            \cp -rf ./.vimrc ~/ &>/dev/null
+            \cp -rf ./* ~/.vim/ &>/dev/null
+        else
+            echo "需要手动安装ctags"
+        fi
+    fi
+
+fi
