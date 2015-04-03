211d846475992d39002ad9b652fbcd99861bcd36
zyfjeff <445188383@qq.com>
2015-04-03 19:12:18 +0100

    更新安装脚本bug

--- install.sh
+++ install.sh
@@ -7,29 +7,31 @@ if [ -e "/etc/redhat-release" ];then
     if [ ! $? -eq 0 ];then
         #不存在ctags可以直接安装
         yum install ctags -y
-        if [ $? -eq 0 ];then
-            mkdir -p ~/.vim/ &>/dev/null
-            \cp -rf ./.vimrc ~/ &>/dev/null
-            \cp -rf ./* ~/.vim/ &>/dev/null
-            echo -e "\033[42m恭喜您安装完成\033[0m"
-        else
-            echo -e "\033[31m安装失败!需要手动安装ctags\033[0m"
-        fi
-
     fi
+
+    if [ $? -eq 0 ];then
+        mkdir -p ~/.vim/ &>/dev/null
+        \cp -rf ./.vimrc ~/ &>/dev/null
+        \cp -rf ./* ~/.vim/ &>/dev/null
+        echo -e "\033[42m恭喜您安装完成\033[0m"
+    else
+        echo -e "\033[31m安装失败!需要手动安装ctags\033[0m"
+    fi
+
 else
     #是 ubuntu系列
     which ctags &>/dev/null
     if [ ! $? -eq 0 ];then
         sudo apt-get install ctags -y
-        if [ $? -eq 0 ];then
-            mkdir -p ~/.vim/ &>/dev/null
-            \cp -rf ./.vimrc ~/ &>/dev/null
-            \cp -rf ./* ~/.vim/ &>/dev/null
+    fi
+
+    if [ $? -eq 0 ];then
+        mkdir -p ~/.vim/ &>/dev/null
+        \cp -rf ./.vimrc ~/ &>/dev/null
+        \cp -rf ./* ~/.vim/ &>/dev/null
             echo -e "\033[42m恭喜您安装完成\033[0m"
-        else
+    else
             echo -e "\033[31m安装失败!需要手动安装ctags\033[0m"
-        fi
     fi
 
 fi
